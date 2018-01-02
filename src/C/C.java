46e9929f3c3f73476a0f262be2ac4cefc73010e2
chanJP <2535482507@qq.com>
2018-01-02 16:59:01 +0800

    6564

---
+++ src/C/C.java
@@ -0,0 +1,19 @@
+package C;
+import java.util.Scanner;
+class C{
+
+	public static void main(String[] args) {
+		Scanner  sc=new Scanner(System.in);
+System.out.println("请输出一个数");
+		int b=sc.nextInt();
+	 for(int n=1;n<=b;n++){
+	
+	System.out.println(n);
+	
+	}
+	
+	}
+	
+}
+
+
